573c4cce12042cc49a3f873a246878af7433ea2a
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2020-03-07 16:58:41 +0200

    Initial

---
+++ RoyalTravel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoyalTravel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
