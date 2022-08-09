ee1f2209550733139dada5086ee62abf97098420
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-08-09 21:10:33 +0300

    [UPDATE FRAMEWORK]  From 3.1 to 6.0 , upgrade packages

--- RoyalTravel/Controllers/HomeController.cs
+++ RoyalTravel/Controllers/HomeController.cs
@@ -1,14 +1,8 @@
 ﻿namespace RoyalTravel.Controllers
 {
-    using System;
-    using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.Logging;
     using RoyalTravel.ViewModels;
-    using Stripe;
 
     public class HomeController : Controller
     {
--- RoyalTravel/Startup.cs
+++ RoyalTravel/Startup.cs
@@ -1,16 +1,9 @@
 namespace RoyalTravel
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.HttpsPolicy;
     using Microsoft.AspNetCore.Identity;
-    using Microsoft.AspNetCore.Identity.UI;
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.Razor;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
@@ -21,13 +14,12 @@ namespace RoyalTravel
     using RoyalTravel.Services.Room;
     using RoyalTravel.Services.Stays;
     using RoyalTravel.Services.User;
-    using Stripe;
 
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -82,9 +74,12 @@ namespace RoyalTravel
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
+                // The default HSTS value is 30 days. You may want to change this for production scenarios,
+                // see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -98,9 +93,11 @@ namespace RoyalTravel
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapAreaControllerRoute(name: "areas", "areas", pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}");
-
-
+                endpoints
+                .MapAreaControllerRoute(
+                  name: "areas",
+                  areaName: "areas",
+                  pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
 
             });
